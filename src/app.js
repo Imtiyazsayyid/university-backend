4ffa00bb6034f9d92952dbf8b8b6607bf3ff04b6
Imtiyaz Sayyid <imtiyazsayyidwork@gmail.com>
2025-01-31 00:12:50 +0530

    Fix Cors

--- src/app.js
+++ src/app.js
@@ -12,7 +12,39 @@ app.set("port", process.env.PORT || 8003);
 app.use(express.static("public"));
 app.use(bodyParser.json({ limit: "500mb" }));
 
-app.use(cors());
+const allowedOrigins = [
+  "https://classifi-admin-stg.imtiyazsayyid.in",
+  "https://classifi-teacher-stg.imtiyazsayyid.in",
+  "https://classifi-student-stg.imtiyazsayyid.in",
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "http://localhost:3002",
+  "http://localhost:3003",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true); // Allow the request
+      } else {
+        callback(new Error("Not allowed by CORS")); // Reject the request
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "Access-Control-Allow-Origin", // You can add custom headers here, if needed
+    ],
+    credentials: true,
+  })
+);
+
+// Handle preflight requests
+app.options("*", cors());
+
 app.use(cookieParser());
 app.use(
   fileUpload({
